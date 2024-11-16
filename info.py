5cf4ee81a3a1a002e371cd364d00c682dd326d0c
Codaangi <121353390+Jegaa1@users.noreply.github.com>
2024-11-16 07:24:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,7 @@ else:
     LOG_CHANNEL = int(LOG_CHANNEL)
 
 # support group
-SUPPORT_GROUP = environ.get('SUPPORT_GROUP', 'https://t.me/moviekoodu')
+SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '-1002133392581')
 if len(SUPPORT_GROUP) == 0:
     print('Error - SUPPORT_GROUP is missing, exiting now')
     exit()
